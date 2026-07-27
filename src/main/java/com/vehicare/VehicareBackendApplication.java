43112b4da86f7b185360dbb5d1295c91e9a6c4f2
chetan magar <sonumagar7264@gmail.com>
2026-07-27 18:29:30 +0530

    Initial Commit

---
+++ src/main/java/com/vehicare/VehicareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vehicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vehicare/VehicareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
