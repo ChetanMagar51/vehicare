241fd2c85af4c6dff8d9315307277f98764cec56
chetan magar <sonumagar7264@gmail.com>
2026-09-04 04:43:42 +0530

    chor: create common module

---
+++ src/main/java/com/vehicare/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.vehicare.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.vehicare.common.api.ApiResponse;
+import com.vehicare.modules.user.exception.EmailAlreadyExistsException;
+import com.vehicare.modules.user.exception.UserNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ApiResponse<Void>> handleEmailAlreadyExists(
+            EmailAlreadyExistsException ex) {
+
+        ApiResponse<Void> response = ApiResponse.<Void>builder()
+                .status("Failed")
+                .message(ex.getMessage())
+                .data(null)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(response);
+    }
+    
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleUserNotFoundException(UserNotFoundException ex)
+    {
+    	  
+    	
+    return	new ResponseEntity<>(ApiResponse.<Void>builder()
+                .message(ex.getMessage())
+                .status(HttpStatus.NOT_FOUND.name())
+                .build(), HttpStatus.NOT_FOUND);
+    	
+    }
+    
+    
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiResponse<Void>> handleBadCredentials(
+            BadCredentialsException ex) {
+
+        ApiResponse<Void> response = ApiResponse.<Void>builder()
+                .status("Failed")
+                .message("Invalid email or password")
+                .data(null)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(response);
+    }
+}
