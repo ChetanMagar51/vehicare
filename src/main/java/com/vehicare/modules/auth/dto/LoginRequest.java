b8cdf6372ac8429106dc66198879b9efc9fb4a45
chetan magar <sonumagar7264@gmail.com>
2026-09-18 15:05:20 +0530

    refactor: update auth DTO

--- src/main/java/com/vehicare/modules/auth/dto/LoginRequest.java
+++ src/main/java/com/vehicare/modules/auth/dto/LoginRequest.java
@@ -1,13 +1,18 @@
 package com.vehicare.modules.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class LoginRequest {
-	
+	@NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
 	String email;
+	
+	@NotBlank(message = "Password is required")
     String password;
 
 }
