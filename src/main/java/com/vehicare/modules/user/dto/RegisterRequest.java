c01d60a986d8e106f651a99ee5fb0756c3f85b78
chetan magar <sonumagar7264@gmail.com>
2026-09-18 15:07:34 +0530

    refactor: update user dto and entity

--- src/main/java/com/vehicare/modules/user/dto/RegisterRequest.java
+++ src/main/java/com/vehicare/modules/user/dto/RegisterRequest.java
@@ -1,5 +1,10 @@
 package com.vehicare.modules.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,12 +15,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
-	
-	private String firstName;
-    private String lastName;
-    private String phone;
-    private String address;
-    private String email;
-    private String password;
 
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+        regexp = "^[0-9]{10}$",
+        message = "Phone number must contain 10 digits"
+    )
+    private String phone;
+
+    private String address;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must contain at least 8 characters")
+    private String password;
 }
