d76f9e994850181829ae9476e6e7e31b81ff8b1d
chetan magar <sonumagar7264@gmail.com>
2026-09-20 15:53:05 +0530

    refactor: appointment exception handler name change

--- src/main/java/com/vehicare/modules/appointment/exception/appointmentExceptionHandler.java
+++ src/main/java/com/vehicare/modules/appointment/exception/appointmentExceptionHandler.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.vehicare.common.api.ApiResponse;
 
 @RestControllerAdvice
-public class appointmentExceptionHandler {
+public class AppointmentExceptionHandler {
 
 	@ExceptionHandler(AppointmentNotFoundException.class)
 	public ResponseEntity<ApiResponse<Void>> profileAlreadyExistsException(AppointmentNotFoundException ex) {
 		ApiResponse<Void> response = ApiResponse.<Void>builder().status("Failed").message(ex.getMessage()).data(null)
 				.build();
 
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
 	}
 
 }
