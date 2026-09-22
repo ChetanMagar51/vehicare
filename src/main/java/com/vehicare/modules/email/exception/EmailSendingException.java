0ff673f55bfafb7cd6a7cb6274a9fbd331ec9da0
chetan magar <sonumagar7264@gmail.com>
2026-09-22 18:13:14 +0530

    update: add constructor

--- src/main/java/com/vehicare/modules/email/exception/EmailSendingException.java
+++ src/main/java/com/vehicare/modules/email/exception/EmailSendingException.java
@@ -5,4 +5,8 @@ public class EmailSendingException extends RuntimeException {
     public EmailSendingException(String message, Throwable cause) {
         super(message, cause);
     }
+
+	public EmailSendingException(String message) {
+		super(message);
+	}
 }
